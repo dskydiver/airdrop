1a6c0aa607080616d4b7d72e1a15dafe5e88e441
dskydiver <109866033+dskydiver@users.noreply.github.com>
2024-04-16 08:59:35 -0500

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -15,17 +15,8 @@ apt-get install -y \
   libudev-dev
 
 ## Install rustup and common components
-curl https://sh.rustup.rs -sSf | sh -s -- -y 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
-
-rustup install nightly
-rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
-rustup component add clippy 
-rustup component add clippy --toolchain nightly
-
-cargo install cargo-expand
-cargo install cargo-edit
 
 ## setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -33,6 +24,10 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
+## install firacode
+sudo add-apt-repository universe
+sudo apt-get install fonts-firacode
 
 # install solana
 sh -c "$(curl -sSfL https://release.solana.com/v1.18.9/install)"
